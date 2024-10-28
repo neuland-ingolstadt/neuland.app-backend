4a4211c5806cc22b24362156fdf20fa470d0b670
Robert Eggl <github@roberteggl.dev>
2024-10-28 12:19:23 +0100

    refactor(database): remove unused food schema and related migrations; add room report reason type and table

--- src/db/migrations/0006_true_black_panther.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "room_reports" ALTER COLUMN "description" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "room_reports" ADD COLUMN "resolved_at" timestamp with time zone;
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,11 +1,9 @@
 import * as appAnnouncements from '@/db/schema/appAnnouncements'
-import * as food from '@/db/schema/food'
 import * as roomReports from '@/db/schema/roomReports'
 import * as universitySports from '@/db/schema/universitySports'
 
 export default {
     ...appAnnouncements,
     ...universitySports,
-    ...food,
     ...roomReports,
 }
