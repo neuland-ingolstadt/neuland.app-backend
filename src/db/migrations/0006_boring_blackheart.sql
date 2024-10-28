dc4d9472e99def3c65f116bd6fb91857342bee91
Robert Eggl <github@roberteggl.dev>
2024-10-28 16:10:48 +0100

    feat(migrations): recreate app_platform and user_kind types with updated schema for app_announcements

--- src/db/migrations/0006_boring_blackheart.sql
+++ src/db/migrations/0006_boring_blackheart.sql
@@ -1,4 +1,4 @@
 CREATE TYPE "public"."app_platform" AS ENUM('ANDROID', 'IOS', 'WEB', 'WEB_DEV');--> statement-breakpoint
 CREATE TYPE "public"."user_kind" AS ENUM('STUDENT', 'EMPLOYEE', 'GUEST');--> statement-breakpoint
-ALTER TABLE "app_announcements" ADD COLUMN "platform" app_platform[] NOT NULL;
+ALTER TABLE "app_announcements" ADD COLUMN "platform" app_platform[] NOT NULL;--> statement-breakpoint
 ALTER TABLE "app_announcements" ADD COLUMN "user_kind" user_kind[] NOT NULL;
